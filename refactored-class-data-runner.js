fb7e7161a599d28b02cd1c71547f329a4c37ec97
Aditya Sharat <adityasharat@gmail.com>
2019-02-15 15:58:58 +0000

    add new

---
+++ refactored-class-data-runner.js
@@ -0,0 +1,317 @@
+const data = [{
+    "name": "InternalNode",
+    "fields": [
+      "mCachedMeasuresValid",
+      "mDiffNode",
+      "mLastHeightSpec",
+      "mLastMeasuredHeight",
+      "mLastMeasuredWidth",
+      "mLastWidthSpec",
+      "mNestedTreeProps",
+      "mResolvedHeight",
+      "mResolvedTouchExpansionLeft",
+      "mResolvedTouchExpansionRight",
+      "mResolvedWidth",
+      "mResolvedX",
+      "mResolvedY",
+      "mWorkingRangeRegistrations",
+      {
+        "name": "ComponentContext"
+      },
+      {
+        "name": "Component"
+      },
+      {
+        "name": "NodeInfo"
+      },
+      {
+        "name": "YogaNode"
+      },
+      {
+        "name": "InternalNode.OtherProps"
+      },
+      {
+        "name": "CommonPropsHolder"
+      }
+    ]
+  },
+  {
+    "name": "InternalNode.OtherProps",
+    "fields": [
+      "mTouchExpansion",
+      "mComponentsNeedingPreviousRenderData",
+      "mIsPaddingPercent"
+    ]
+  },
+  {
+    "name": "ComponentContext",
+    "fields": [
+      "mComponentTree",
+      "mContext",
+      "mDefStyleAttr",
+      "mDefStyleRes",
+      "mHeightSpec",
+      "mKeyHandler",
+      "mLogger",
+      "mLogTag",
+      "mNoStateUpdatesMethod",
+      "mResourceCache",
+      "mStateHandler",
+      "mTreeProps",
+      "mWidthSpec",
+      {
+        "name": "Component"
+      }
+    ]
+  },
+  {
+    "name": "Component",
+    "fields": [
+      "mChildCounters",
+      "mErrorEventHandler",
+      "mGlobalKey",
+      "mHasManualKey",
+      "mId",
+      "mIsLayoutStarted",
+      "mIsNestedTreeResolutionExperimentEnabled",
+      "mKey",
+      "mLayoutCreatedInWillRender",
+      "mLayoutVersionGenerator",
+      "mManualKeys",
+      "mOwnerGlobalKey",
+      "mSimpleName",
+      "mSplitChildrenLayoutInThreadPool",
+      "mThreadIdToLastMeasuredLayout",
+      "mWorkingRangeRegistrations",
+      {
+        "name": "ComponentContext"
+      },
+      {
+        "name": "CommonPropsHolder"
+      }
+    ]
+  },
+  {
+    "name": "CommonPropsHolder",
+    "fields": [
+      "mBackground",
+      "mDefStyleAttr",
+      "mDefStyleRes",
+      "mPositions",
+      "mPositionType",
+      {
+        "name": "NodeInfo"
+      },
+      {
+        "name": "CommonPropsHolder.DebugProps"
+      },
+      {
+        "name": "CommonPropsHolder.FlexProps"
+      },
+      {
+        "name": "CommonPropsHolder.LayoutProps"
+      },
+      {
+        "name": "CommonPropsHolder.LayoutPropsInPercentage"
+      },
+      {
+        "name": "CommonPropsHolder.OtherLayoutProps"
+      },
+      {
+        "name": "CommonPropsHolder.OtherProps"
+      },
+      {
+        "name": "CommonPropsHolder.TransitionProps"
+      },
+      {
+        "name": "CommonPropsHolder.VisibilityHandlers"
+      }
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.DebugProps",
+    "fields": [
+      "mDebugComponents",
+      "mTestKey"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.FlexProps",
+    "fields": [
+      "mAlignSelf",
+      "mFlex",
+      "mFlexBasisPx",
+      "mFlexGrow",
+      "mFlexShrink",
+      "mLayoutDirection"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.LayoutProps",
+    "fields": [
+      "mHeightPx",
+      "mMargins",
+      "mPaddings",
+      "mWidthPx"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.LayoutPropsInPercentage",
+    "fields": [
+      "mFlexBasisPercent",
+      "mHeightPercent",
+      "mMarginPercents",
+      "mMaxHeightPercent",
+      "mMaxWidthPercent",
+      "mMinHeightPercent",
+      "mMinWidthPercent",
+      "mPaddingPercents",
+      "mPositionPercents",
+      "mWidthPercent"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.OtherLayoutProps",
+    "fields": [
+      "mAspectRatio",
+      "mIsReferenceBaseline",
+      "mMarginAutos",
+      "mMaxHeightPx",
+      "mMaxWidthPx",
+      "mMinHeightPx",
+      "mMinWidthPx",
+      "mTouchExpansions"
+    ]
+  },
+  {
+    "name": "OtherProps",
+    "fields": [
+      "mBorder",
+      "mDuplicateParentState",
+      "mForeground",
+      "mImportantForAccessibility",
+      "mStateListAnimator",
+      "mStateListAnimatorRes",
+      "mUseHeightAsBaseline",
+      "mWrapInView"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.TransitionProps",
+    "fields": [
+      "mTransitionKey",
+      "mTransitionKeyType",
+      "mTransitions"
+    ]
+  },
+  {
+    "name": "CommonPropsHolder.VisibilityHandlers",
+    "fields": [
+      "mFocusedHandler",
+      "mFullImpressionHandler",
+      "mInvisibleHandler",
+      "mUnfocusedHandler",
+      "mVisibilityChangedHandler",
+      "mVisibleHandler",
+      "mVisibleHeightRatio",
+      "mVisibleWidthRatio"
+    ]
+  },
+  {
+    "name": "NodeInfo",
+    "fields": [
+      "mBackground",
+      "mExpandedTouchBounds",
+      "mForeground",
+      "mLayoutDirection",
+      "mPadding",
+      "mStateListAnimator",
+      "mStateListAnimatorRes"
+    ]
+  },
+  {
+    "name": "YogaNode",
+    "fields": [
+      "mBaselineFunction",
+      "mBorderBottom",
+      "mBorderLeft",
+      "mBorderRight",
+      "mBorderTop",
+      "mChildren",
+      "mDoesLegacyStretchFlagAffectsLayout",
+      "mEdgeSetFlag",
+      "mHasNewLayout",
+      "mHasSetPosition",
+      "mHeight",
+      "mLayoutDirection",
+      "mLeft",
+      "mMarginBottom",
+      "mMarginLeft",
+      "mMarginRight",
+      "mMarginTop",
+      "mMeasureFunction",
+      "mNativePointer",
+      "mOwner",
+      "mPaddingBottom",
+      "mPaddingLeft",
+      "mPaddingRight",
+      "mPaddingTop",
+      "mTop",
+      "mWidth",
+      {
+        "name": "InternalNode"
+      }
+    ]
+  }
+];
+
+const links = [];
+const nodes = [];
+const map = {};
+
+let counter = 0;
+
+function process(node, source) {
+  let name;
+  let type;
+  let id;
+  if (typeof node === 'string') {
+    name = node;
+    type = 'property'
+  } else {
+    name = node.name;
+    type = 'class'
+  }
+
+  if (!map[name]) {
+    id = counter;
+    nodes.push({
+      id,
+      name,
+      type
+    });
+    map[name] = id;
+    counter++;
+  } else {
+    id = map[name];
+  }
+
+  if (typeof source === 'number') {
+    links.push({
+      source,
+      target: id
+    });
+  }
+
+  return id;
+}
+
+data.forEach(c => {
+  const id = process(c, null);
+  c.fields.forEach(f => process(f, id));
+});
+
+const result = {
+  nodes,
+  links
+};
